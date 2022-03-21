eac2ac5a779ab04c234eb959e53d44c996279230
Laye 'Leo' Mehta <layeamehta@gmail.com>
2022-03-21 14:37:58 -0400

    added winningLogic and handleClick in Game.js

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -4,7 +4,6 @@ import Game from './components/Game';
 function App() {
   return (
     <div className="App">
-      <h1>Laye</h1>
       <Game></Game>
     </div>
   );
--- tic-tac-toe/src/components/Game.js
+++ tic-tac-toe/src/components/Game.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Board from './Board'; 
+import {calculateWinner} from '../winningLogic'; 
 
 
 function Game() {
+    // history is 2D array, meant to store array of squares for every valid move. 
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [stepNumber, setStepNumber] = useState(0);
+    const [xIsNext, setxIsNext] = useState(true);
+    const winner = calculateWinner(history[stepNumber]);
+    const xO = xIsNext ? "X" : "O";  
+
+    const handleClick = (i) => {
+        // slice history up until and including current stepNumber
+        const historyPoint = history.slice(0, stepNumber+1);
+        // get current steps array, and copy it into squares variable
+        const current = historyPoint[stepNumber]; 
+        const squares = [...current]; 
+
+        // return if winner is already declared, or squares[i] has existing character
+        if(winner || squares[i]) return; 
+
+        // update squares array, and add new array to history
+        squares[i] = xO; 
+        setHistory([...historyPoint, squares]); 
+        setStepNumber(historyPoint.length); 
+        setxIsNext(!xIsNext); 
+    }
+
+    const jumpTo = (step) => {
+        setStepNumber(step); 
+        setxIsNext(step%2 == 0); 
+    }
+
+
     return ( 
-        <div>
-        <Board squares={[]}></Board>
-        </div>
+        <>
+        <h1>Tic Tac Toe Game</h1>
+        <Board squares={history[stepNumber]} onClick={handleClick}></Board>
+        </>
      );
 }
 
